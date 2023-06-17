62a94146eb373ef93fe77a8a35c6e21ef291ae5f
microcoded <16248692+microcoded@users.noreply.github.com>
2023-06-17 14:59:59 +1000

    Add files via upload

---
+++ Text Selection Everywhere.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        Text Selection Everywhere
+// @version     1.0
+// @description Stop websites from blocking text selection – copy and paste away!
+// @grant       GM_addStyle
+// @match       *://*/*
+// @run-at      document-idle
+// ==/UserScript==
+
+GM_addStyle('*{ -webkit-user-select:auto!important;-ms-user-select:auto!important;user-select:auto!important;');
