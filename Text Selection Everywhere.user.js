4b1125fb47b3a17af5409a20c4ef403ac79327ad
microcoded <16248692+microcoded@users.noreply.github.com>
2023-06-17 15:03:09 +1000

    Add author tag

--- Text Selection Everywhere.user.js
+++ Text Selection Everywhere.user.js
@@ -1,5 +1,6 @@
 // ==UserScript==
 // @name        Text Selection Everywhere
+// @author      microcoded (https://github.com/microcoded)
 // @version     1.0
 // @description Stop websites from blocking text selection – copy and paste away!
 // @grant       GM_addStyle
